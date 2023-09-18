f5132f2d2d7443bb163c767bf93bb92dbd8871e4
HP <HP@AGASTI-PC>
2023-09-18 15:42:55 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Thread SecondaryThread = new Thread(new ThreadStart(print));
+SecondaryThread.Start();
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Primary Thread:{i}");
+    Thread.Sleep(1000);
+}
+
+
+Thread t;
+t = Thread.CurrentThread;
+Console.WriteLine("Thread is Alive or Not: {0}", t.IsAlive);
+Thread.Sleep(10000);
+
+
+Console.ReadKey();
+
+static void print()
+{
+    for (int i = 11; i < 20; i++)
+    {
+        Console.WriteLine($"SecondaryThread:{i}");
+        Thread.Sleep(1000);
+    }
+}
